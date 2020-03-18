0e5a5818426c3128f24d9dc701a7d772a928c246
Matthew <matthewvella29@gmail.com>
2020-03-18 19:09:35 +0100

    initial commit for weatherapi setup

---
+++ WeatherWebClient/WeatherWebClient/Models/AccuWeatherAPILocationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace WeatherWebClient.Models
+{
+    [DataContract]
+    class AccuWeatherAPILocationModel
+    {
+        [DataMember]
+        public string Key { get; set; }
+    }
+}
---
+++ WeatherWebClient/WeatherWebClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using WeatherWebClient.Controller;
+using WeatherWebClient.POCO;
+
+namespace WeatherWebClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OpenWeatherMapAPI();
+            AccuWeatherAPI();
+            Console.ReadKey();
+        }
+
+        private static void OpenWeatherMapAPI()
+        {
+            string cityName = "Valletta";
+
+            /**** Open Weather Map ****/
+            /**** Current Weather ****/
+            OpenWeatherMapController openWeatherMapController = new OpenWeatherMapController();
+
+            Console.WriteLine("***** Open Weather Map *****");
+            Console.WriteLine("***** Current Weather API *****");
+            Console.WriteLine($"Current Temperature for {cityName}: {openWeatherMapController.getCurrentWeather(cityName)}");
+
+            /**** FORECAST****/
+            Console.WriteLine("***** Forecast API *****");
+            Console.WriteLine($"Forecast for {cityName}: ");
+            foreach (OpenWeatherMapForecast forecast in openWeatherMapController.getForecast(cityName))
+            {
+                Console.WriteLine($"{forecast.getDateTime().ToString()} Temperature: {forecast.getTemperature()}");
+            }
+        }
+
+        private static void AccuWeatherAPI()
+        {
+            string cityName = "Valletta";
+
+            /**** AccuWeather ****/
+            /**** Current Weather ****/
+            AccuWeatherController accuWeatherController = new AccuWeatherController();
+
+            Console.WriteLine("***** AccuWeather *****");
+            Console.WriteLine("***** Current Weather API *****");
+            Console.WriteLine($"Current Temperature for {cityName}: {accuWeatherController.getCurrentWeather(cityName)}");
+
+            /**** FORECAST****/
+            Console.WriteLine("***** Forecast API *****");
+            Console.WriteLine($"Forecast for {cityName}: ");
+            foreach (AccuWeatherForecast forecast in accuWeatherController.getForecast(cityName))
+            {
+                Console.WriteLine($"{forecast.getDateTime().ToString()} Minimum: {forecast.getMinimum()} Maximum: {forecast.getMaximum()}");
+            }
+
+        }
+    }
+}
